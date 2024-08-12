6fd2710ab94d93f6a577534b8f16361537e6af6d
Curtis.Novak <Curtis.Novak@garmin.com>
2024-08-12 07:45:33 -0500

    Version 4.3.2g

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("4.3.2")]
 [assembly: AssemblyFileVersion("4.3.2")]
-[assembly: AssemblyInformationalVersion("4.3.2f")]
+[assembly: AssemblyInformationalVersion("4.3.2g")]
 
 // Disable CLS compliance. See https://github.com/gitextensions/gitextensions/issues/4710
 [assembly: CLSCompliant(isCompliant: false)]
