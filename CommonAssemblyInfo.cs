b84e5310e66ecbc75210261d258163ccbee52def
Curtis.Novak <Curtis.Novak@garmin.com>
2025-05-14 06:42:04 -0500

    Version 5.3.2d
    First build with VS 17.14.0

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("5.3.2")]
 [assembly: AssemblyFileVersion("5.3.2")]
-[assembly: AssemblyInformationalVersion("5.3.2c")]
+[assembly: AssemblyInformationalVersion("5.3.2d")]
 
 // Disable CLS compliance. See https://github.com/gitextensions/gitextensions/issues/4710
 [assembly: CLSCompliant(isCompliant: false)]
