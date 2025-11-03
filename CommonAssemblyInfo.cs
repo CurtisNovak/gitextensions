befcc957d64d2fe963d8502afa9e6943831e9bd0
Curtis.Novak <Curtis.Novak@garmin.com>
2025-11-03 06:18:47 -0600

    Version 6.1.0j
    Change-Id: I42a47d83da30c40dbf6a1f19e9a6065a587b3f3b

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("6.1.0")]
 [assembly: AssemblyFileVersion("6.1.0")]
-[assembly: AssemblyInformationalVersion("6.1.0i")]
+[assembly: AssemblyInformationalVersion("6.1.0j")]
 
 // Disable CLS compliance. See https://github.com/gitextensions/gitextensions/issues/4710
 [assembly: CLSCompliant(isCompliant: false)]
