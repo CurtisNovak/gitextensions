a8ea0ff1a08d7d5e47dce9e35c4395e2201ccb46
Curtis.Novak <Curtis.Novak@garmin.com>
2025-11-17 06:46:53 -0600

    Version 6.0.4b
    Change-Id: I65607bd49374f176d6341f182e517d5defff8303

--- CommonAssemblyInfoExternals.cs
+++ CommonAssemblyInfoExternals.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("6.0.4")]
 [assembly: AssemblyFileVersion("6.0.4")]
-[assembly: AssemblyInformationalVersion("6.0.4a")]
+[assembly: AssemblyInformationalVersion("6.0.4b")]
 
 // Disable CLS compliance. See https://github.com/gitextensions/gitextensions/issues/4710
 [assembly: CLSCompliant(isCompliant: false)]
